ebfa6b77d4afb964de946abebea80e14e5e50b16
samritigupta <gupta.samriti1991@gmail.com>
2020-12-12 17:56:53 +0000

    code refactoring

---
+++ src/main/java/com/springmvclearn/basic/controller/StudentController.java
@@ -0,0 +1,54 @@
+package com.springmvclearn.basic.controller;
+
+import java.util.Map;
+
+import com.springmvclearn.basic.entity.Student;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@Api(tags = "Student")
+@RequestMapping("/student")
+public class StudentController {
+
+	@Value("#{languageOptions}")
+	private Map<String, String> languageOptions;
+
+	@RequestMapping(value = "/showForm", method = RequestMethod.GET)
+	public String showForm(Model theModel) {
+		
+		// create a student object
+		Student theStudent = new Student();
+		
+		// add student object to the model
+		theModel.addAttribute("student", theStudent);
+		theModel.addAttribute("theLanguageOptions", languageOptions);
+		
+		return "student-form";
+	}
+	
+	@RequestMapping("/processForm")
+	public String processForm(@ModelAttribute("student") Student theStudent) {
+		
+		// log the input data
+		System.out.println("theStudent: " + theStudent.getFirstName()
+							+ " " + theStudent.getLastName());
+		
+		return "student-confirmation";
+	}
+	
+}
+
+
+
+
+
+
+
+
+
